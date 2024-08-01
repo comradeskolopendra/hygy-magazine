dfed6290618281b34a0f8919a97a1892b3d08eb5
comradeskolopendra <comradeskolopendra@gmail.com>
2024-08-01 12:59:25 +0500

    Добавить правки по top-news

--- src/pages/home/components/top-news/article/article.tsx
+++ src/pages/home/components/top-news/article/article.tsx
@@ -12,9 +12,9 @@ const Article: FC<ArticleProps> = ({ image, title, description }) => {
     return (
         <article className={styles.article}>
 
-            {/* <div className={styles.imageWrapper}> */}
-            <img src={image} alt="" />
-            {/* </div> */}
+            <div className={styles.imageWrapper}>
+                <img src={image} alt="" />
+            </div>
 
             <div className={styles.info}>
                 <h3 className={styles.title}>{title}</h3>
